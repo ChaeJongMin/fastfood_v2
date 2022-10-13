f69ef79b75b1b9af3c5c61ca17bd7386ad1ba5b3
Chae JongMin <dico2760@naver.com>
2022-10-13 21:08:45 +0900

    변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl fastfood_spring_webservice | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl fastfoodweb | grep jar | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid : $CURRENT_PID"
 
