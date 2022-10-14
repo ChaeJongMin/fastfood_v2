638a89361d528183214f731f9d8cea1016de5bd4
Chae JongMin <dico2760@naver.com>
2022-10-14 21:28:58 +0900

    변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,6 +10,8 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
 CURRENT_PID=$(pgrep -fl fastfood_up | grep jar | awk '{print $1}')
+CURRENT_PID2=$(pgrep -fl fastfood-spring2-webservice | grep jar | awk '{print $1}')
+CURRENT_PID3=$(pgrep -fl fastfood-spring-webservice | grep jar | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid : $CURRENT_PID"
 
@@ -21,6 +23,21 @@ else
     sleep 5
 fi
 
+if [ -z "$CURRENT_PID2" ]; then
+    echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+    echo "> kill -15 $CURRENT_PID2"
+    kill -15 $CURRENT_PID2
+    sleep 5
+fi
+
+if [ -z "$CURRENT_PID3" ]; then
+    echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+    echo "> kill -15 $CURRENT_PID3"
+    kill -15 $CURRENT_PID3
+    sleep 5
+fi
 echo "> 새 애플리케이션 배포"
 
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
